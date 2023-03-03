1279a342f05f0b7d57f7c2a9e21331ed20f62330
Victor Tisbierek <metropoller@gmail.com>
2023-03-03 11:27:08 -0300

    removing unnecessary dependencies

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 mongoose.set('strictQuery', false); 
 
